2248a36879971b5f384962979e4c34390cde113e
= <=>
2025-11-04 12:05:39 +0530

    first commit

--- workflow_script_serial.py
+++ workflow_script_serial.py
@@ -162,7 +162,7 @@ def main():
 
         loraloadermodelonly = LoraLoaderModelOnly()
         loraloadermodelonly_89 = loraloadermodelonly.load_lora_model_only(
-            lora_name="Qwen-Image-Lightning-4steps-V2.0.safetensorss",
+            lora_name="Qwen-Image-Lightning-4steps-V2.0.safetensors",
             strength_model=1,
             model=get_value_at_index(unetloader_37, 0),
         )
